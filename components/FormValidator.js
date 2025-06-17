4da7a2082f7c26e8e6ddfd4aac922b5d9a12d205
Marc Pickelman <marcpc@att.net>
2025-06-17 03:48:35 -0500

    Refactor todo app - nearly complete

---
+++ components/FormValidator.js
@@ -0,0 +1,10 @@
+class FormValidator {
+  constructor(settings, formEl) {
+    //this._settings = settings;
+    //this._formEl = formEl;
+    console.log(settings);
+    console.log(formEl);
+  }
+}
+
+export { FormValidator };
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
+import { v4 as uuidv4 } from "https://jspm.dev/uuid";
+
+import { initialTodos, validationConfig } from "../utils/constants.js";
+import Todo from "../components/Todo.js";
+import { FormValidator } from "../components/FormValidator.js";
+
 const addTodoButton = document.querySelector(".button_action_add");
 const addTodoPopup = document.querySelector("#add-todo-popup");
 const addTodoForm = addTodoPopup.querySelector(".popup__form");
 const addTodoCloseBtn = addTodoPopup.querySelector(".popup__close");
-const todoTemplate = document.querySelector("#todo-template");
+// const todoTemplate = document.querySelector("#todo-template"); -> remove
 const todosList = document.querySelector(".todos__list");
 
 const openModal = (modal) => {
@@ -15,7 +21,12 @@ const closeModal = (modal) => {
 
 // The logic in this function should all be handled in the Todo class.
 const generateTodo = (data) => {
-  const todoElement = todoTemplate.content
+  const todo = new Todo(data, "#todo-template");
+  const todoElement = todo.getView();
+  return todoElement;
+
+  // To be removed:
+  /*const todoElement = todoTemplate.content
     .querySelector(".todo")
     .cloneNode(true);
   const todoNameEl = todoElement.querySelector(".todo__name");
@@ -45,9 +56,7 @@ const generateTodo = (data) => {
 
   todoDeleteBtn.addEventListener("click", () => {
     todoElement.remove();
-  });
-
-  return todoElement;
+  });*/
 };
 
 addTodoButton.addEventListener("click", () => {
@@ -67,7 +76,8 @@ addTodoForm.addEventListener("submit", (evt) => {
   const date = new Date(dateInput);
   date.setMinutes(date.getMinutes() + date.getTimezoneOffset());
 
-  const values = { name, date };
+  const id = uuidv4();
+  const values = { name, date, id };
   const todo = generateTodo(values);
   todosList.append(todo);
   closeModal(addTodoPopup);
@@ -77,3 +87,4 @@ initialTodos.forEach((item) => {
   const todo = generateTodo(item);
   todosList.append(todo);
 });
+
